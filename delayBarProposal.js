e5ce81fcb0d2ee6b127e757b4eff03d65ca53f54
DamienTaubman <33710352+DamienTaubman@users.noreply.github.com>
2020-08-10 01:36:11 +1000

    Update delayBarProposal.js
    moved code inside class to allow constructing

--- delayBarProposal.js
+++ delayBarProposal.js
@@ -1,52 +1,43 @@
-/*
-Delay Bar replacement
-this replacement Delay bar will deplete using an animation frame, and will always ensure
-that the previous animation is cancelled, before starting a new one
-*/
 
-// Define our Variables so we can easily substitute name changes in interface code
-var theDelayBar = BarneyDiv
-var delayLeftHalf = theDelayBar.children[0].rows[0].cells[1]
-var delayRightHalf = theDelayBar.children[0].rows[0].cells[2]
+class RwkDelayBar {
+  constructor() {
+    this.uiElement = BarneyDiv
+    this.leftHalf = this.uiElement.children[0].rows[0].cells[1]
+    this.rightHalf = this.uiElement.children[0].rows[0].cells[2]
+    this.delayBarAnimation
+    this.startTime = ``
+    this.percentRemaining = 0
 
+    this.rightHalf.width = `100%`
+  }
 
-delayRightHalf.width = `100%`
-var delayBarAnimation // we define this to use later
-
-// doDelay will initiate a new delay, with x as the parameter for how long in
-// ms the delay should last (e.g doDelay(500)) will result in a 500 ms delay
-function doDelay(y) {
-  window.cancelAnimationFrame(delayBarAnimation);
-  requestAnimationFrame((timestamp) => {
-    starttime = timestamp || new Date().getTime();
-    processDelay(timestamp, theDelayBar, 100, y, true);
-  });
-}
-
-// updateDelay will insert a new delay timer on an active delay, and resume from
-// where the current one left off
-// i.e if the delay has depleted 50 percent and updateDelay(1000) is called,
-// it will take 1000 ms to deplete the remaining 50 percent
-function updateDelay(y) {
-  window.cancelAnimationFrame(delayBarAnimation);
-  requestAnimationFrame((timestamp) => {
-    starttime = timestamp || new Date().getTime();
-    processDelay(timestamp, theDelayBar, parseInt(percentRemaining), y);
-  });
-}
-
-// Process delay is not designed to be called directly, use one of the helper functions,
-// doDelay, or updateDelay
-function processDelay(timestamp, el, dist, duration, red = 0) {
-  const runtime = timestamp - starttime;
-
-  let progress = runtime / duration;
-  progress = Math.min(progress, 1);
-  percentRemaining = 100 - (progress * 100);
-  delayLeftHalf.attributes.width.nodeValue = `${(dist * (1 - progress)).toFixed(2)}%`
-  if (runtime < duration) {
-    delayBarAnimation = requestAnimationFrame((timestamp) => {
-      processDelay(timestamp, el, dist, duration, red);
+  doDelay(y) {
+    window.cancelAnimationFrame(this.delayBarAnimation);
+    requestAnimationFrame((timestamp) => {
+      this.startTime = timestamp || new Date().getTime();
+      this.processDelay(timestamp, this.uiElement, 100, y, true);
     });
+  }
+
+  updateDelay(y) {
+    window.cancelAnimationFrame(this.delayBarAnimation);
+    requestAnimationFrame((timestamp) => {
+      this.startTime = timestamp || new Date().getTime();
+      this.processDelay(timestamp, this.uiElement, parseInt(this.percentRemaining), y);
+    });
+  }
+
+  processDelay(timestamp, el, dist, duration, red = 0) {
+    const runtime = timestamp - this.startTime;
+
+    let progress = runtime / duration;
+    progress = Math.min(progress, 1);
+    this.percentRemaining = 100 - (progress * 100);
+    this.leftHalf.attributes.width.nodeValue = `${(dist * (1 - progress)).toFixed(2)}%`
+    if (runtime < duration) {
+      this.delayBarAnimation = requestAnimationFrame((timestamp) => {
+        this.processDelay(timestamp, el, dist, duration, red);
+      });
+    }
   }
 }
